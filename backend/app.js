49275828cb6b68356d252b6885d2183ccd90b427
Rob Whitacre <robert.w.whitacre@gmail.com>
2021-02-17 17:58:00 -0600

    error handlers

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,9 @@ const {environment} = require('./config')
 const routes = require('./routes')
 
 const isProduction = environment ==='production'
+const {ValidationError} = require('sequelize')
 const app = express();
+
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(express.json());
@@ -29,5 +31,33 @@ app.use(
   })
 )
 app.use(routes)
+//standard 404 error handler
+app.use((_req, _res, next) => {
+  const err = new Error ("The requested resource could not be found.");
+  err.title = "Resource Not Found";
+  err.errors = ["The requested resource could not be found."];
+  err.status = 404;
+  next(err);
+})
+//Processing the sequelize errors
+app.use((err, _req, _res, next) => {
+  //check if error is actually a sequelize error:
+    if (err instanceof ValidationError) {
+    err.errors = err.errors.map((e) => e.message);
+    err.title = 'Validation error';
+  }
+  next(err);
+})
+//error formatter handler
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack,
+  });
+});
 
 module.exports = app;
