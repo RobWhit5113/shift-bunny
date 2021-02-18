7bad76bbf827d725d419be46125da3b52f426f0e
Rob Whitacre <robert.w.whitacre@gmail.com>
2021-02-18 11:05:59 -0600

    validation middlewares

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -2,10 +2,24 @@ const express = require('express')
 const asyncHandler = require('express-async-handler')
 const{ setTokenCookie, restoreUser} = require('../../utils/auth')
 const{User} = require('../../db/models')
+const {check} = require('express-validator')
+const {handleValidationErrors} = require('../../utils/validation')
 const router = express.Router();
 
+//checking keys of the req.body to validate - middleware
+const validateLogin = [
+  check('credential')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Please provide a valid email or username.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a password.'),
+  handleValidationErrors,
+]
+
 //Log in
-router.post('/', asyncHandler(async(req,res,next) => {
+router.post('/', validateLogin, asyncHandler(async(req,res,next) => {
   const { credential, password} = req.body
 
   const user = await User.login({credential, password})
