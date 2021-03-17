1984970a394623d160ad7cac8a968054726aa9ac
Rob Whitacre <robert.w.whitacre@gmail.com>
2021-03-17 10:50:19 -0400

    edited some of the commented out code that was not used

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -6,24 +6,7 @@ import {useHistory} from 'react-router-dom'
 
 const ProfileButton = ({user}) => {
   const dispatch = useDispatch()
-  // const [showMenu, setShowMenu] = useState(false)
   const history = useHistory()
-
-  // const openMenu = () => {
-  //   if (showMenu) return;
-  //   setShowMenu(true)
-  // }
-  // useEffect(() => {
-  //   if (!showMenu) return 
-
-  //   const closeMenu = () => {
-  //     setShowMenu(false)
-  //   };
-
-  //   document.addEventListener('click', closeMenu)
-
-  //   return () => document.removeEventListener('click', closeMenu)
-  // }, [showMenu])
 
   const logout = (e) => {
     e.preventDefault();
@@ -33,9 +16,6 @@ const ProfileButton = ({user}) => {
 
     return (
     <>
-      {/* <button onClick={openMenu}>
-        <i className="fas fa-user-circle" />
-      </button> */}
       {user && (
         <a onClick={logout} className='nav-span'>log out</a>
       )}
